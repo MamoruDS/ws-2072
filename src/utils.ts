0a990dd312e94e3a044aa979207a7664caf676d7
MamoruDS <mamoruds.io@gmail.com>
2020-09-20 20:21:47 +0900

    rewrote: src/utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,21 @@
-import * as _ from 'lodash'
+const genRandomChar = (radix: number): string => {
+    return Math.floor(Math.random() * radix)
+        .toString(radix)
+        .toLocaleUpperCase()
+}
 
-export const genRandom = (length: number): string => {
-    if (length < 1 || length > 10) {
-        throw new RangeError()
+export const genRandomStr = (len: number): string => {
+    const id = []
+    for (const _ of ' '.repeat(len)) {
+        id.push(genRandomChar(36))
     }
-    return _.random(36 ** (length - 1), 36 ** length - 1).toString(36)
+    return id.join('')
+}
+
+export const genRandomHex = (len: number): string => {
+    const id = []
+    for (const _ of ' '.repeat(len)) {
+        id.push(genRandomChar(16))
+    }
+    return id.join('')
 }
