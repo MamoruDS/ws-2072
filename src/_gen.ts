f3e5010326e7117038260c79ab8bb02cf1d04098
MamoruDS <mamoruds.io@gmail.com>
2020-10-24 15:48:05 +0900

    added: _gen.ts for exporting types generated by script

---
+++ src/_gen.ts
@@ -0,0 +1,133 @@
+export type ASTType = 
+    | 'Add'
+    | 'And'
+    | 'Assert'
+    | 'Assign'
+    | 'AsyncFunctionDef'
+    | 'Attribute'
+    | 'AugAssign'
+    | 'Await'
+    | 'BinOp'
+    | 'BitAnd'
+    | 'BitOr'
+    | 'BoolOp'
+    | 'Break'
+    | 'Call'
+    | 'ClassDef'
+    | 'Compare'
+    | 'Constant'
+    | 'Continue'
+    | 'Del'
+    | 'Delete'
+    | 'Div'
+    | 'Eq'
+    | 'Expr'
+    | 'ExtSlice'
+    | 'For'
+    | 'FormattedValue'
+    | 'FunctionDef'
+    | 'Gt'
+    | 'GtE'
+    | 'If'
+    | 'IfExp'
+    | 'Import'
+    | 'ImportFrom'
+    | 'In'
+    | 'Index'
+    | 'JoinedStr'
+    | 'Lambda'
+    | 'List'
+    | 'ListComp'
+    | 'Load'
+    | 'Lt'
+    | 'LtE'
+    | 'Mod'
+    | 'Module'
+    | 'Mult'
+    | 'Name'
+    | 'Not'
+    | 'NotEq'
+    | 'Or'
+    | 'Pass'
+    | 'Return'
+    | 'Slice'
+    | 'Store'
+    | 'Sub'
+    | 'Subscript'
+    | 'Tuple'
+    | 'USub'
+    | 'UnaryOp'
+    | '_LiteAST.Module'
+    | 'alias'
+    | 'arg'
+    | 'arguments'
+    | 'comprehension'
+    | 'keyword'
+
+
+export const ASTTypeList: ASTType[] = [
+    'Add',
+    'And',
+    'Assert',
+    'Assign',
+    'AsyncFunctionDef',
+    'Attribute',
+    'AugAssign',
+    'Await',
+    'BinOp',
+    'BitAnd',
+    'BitOr',
+    'BoolOp',
+    'Break',
+    'Call',
+    'ClassDef',
+    'Compare',
+    'Constant',
+    'Continue',
+    'Del',
+    'Delete',
+    'Div',
+    'Eq',
+    'Expr',
+    'ExtSlice',
+    'For',
+    'FormattedValue',
+    'FunctionDef',
+    'Gt',
+    'GtE',
+    'If',
+    'IfExp',
+    'Import',
+    'ImportFrom',
+    'In',
+    'Index',
+    'JoinedStr',
+    'Lambda',
+    'List',
+    'ListComp',
+    'Load',
+    'Lt',
+    'LtE',
+    'Mod',
+    'Module',
+    'Mult',
+    'Name',
+    'Not',
+    'NotEq',
+    'Or',
+    'Pass',
+    'Return',
+    'Slice',
+    'Store',
+    'Sub',
+    'Subscript',
+    'Tuple',
+    'USub',
+    'UnaryOp',
+    '_LiteAST.Module',
+    'alias',
+    'arg',
+    'arguments',
+    'comprehension',
+    'keyword'
+]
