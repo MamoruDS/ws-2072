17ab43d2c8a3617576ee7a14b21ccf287663de88
MamoruDS <mamoruds.io@gmail.com>
2021-03-18 04:23:39 +0900

    updated: ast_nodes.py refomatted with black

--- scripts/ast_nodes.py
+++ scripts/ast_nodes.py
@@ -10,33 +10,76 @@ def ast2json(node):
     global ast_count
     ast_count = 0
     if not isinstance(node, AST):
-        raise TypeError('expected AST, got %r' % node.__class__.__name__)
+        raise TypeError(
+            "expected AST, got %r"
+            % node.__class__.__name__
+        )
 
     def _format(node):
         if isinstance(node, AST):
-            fields = [('node', _format(node.__class__.__name__))]
-            _attrs = ['lineno', 'col_offset']
+            fields = [
+                (
+                    "node",
+                    _format(node.__class__.__name__),
+                )
+            ]
+            _attrs = ["lineno", "col_offset"]
             try:
-                fields += [('lineno', node.lineno)]
-                fields += [('end_lineno', node.end_lineno)]
-                fields += [('col_offset', node.col_offset)]
-                fields += [('end_col_offset', node.end_col_offset)]
+                fields += [("lineno", node.lineno)]
+                fields += [
+                    ("end_lineno", node.end_lineno)
+                ]
+                fields += [
+                    ("col_offset", node.col_offset)
+                ]
+                fields += [
+                    (
+                        "end_col_offset",
+                        node.end_col_offset,
+                    )
+                ]
             except:
                 pass
             try:
-                fields += [('lineno', node.body[0].lineno)]
-                fields += [('end_lineno', node.body[0].end_lineno)]
-                fields += [('col_offset', node.body[0].col_offset)]
-                fields += [('end_col_offset', node.body[0].end_col_offset)]
+                fields += [
+                    ("lineno", node.body[0].lineno)
+                ]
+                fields += [
+                    (
+                        "end_lineno",
+                        node.body[0].end_lineno,
+                    )
+                ]
+                fields += [
+                    (
+                        "col_offset",
+                        node.body[0].col_offset,
+                    )
+                ]
+                fields += [
+                    (
+                        "end_col_offset",
+                        node.body[0].end_col_offset,
+                    )
+                ]
             except:
                 pass
-            fields += [(a, _format(b)) for a, b in iter_fields(node)]
+            fields += [
+                (a, _format(b))
+                for a, b in iter_fields(node)
+            ]
             global ast_count
             ast_count = ast_count + 1
-            return '{ %s }' % ', '.join(
-                ('"%s": %s' % field for field in fields))
+            return "{ %s }" % ", ".join(
+                (
+                    '"%s": %s' % field
+                    for field in fields
+                )
+            )
         if isinstance(node, list):
-            return '[ %s ]' % ', '.join([_format(x) for x in node])
+            return "[ %s ]" % ", ".join(
+                [_format(x) for x in node]
+            )
         node = str(node)
         return json.dumps(node)
 
@@ -53,18 +96,27 @@ def iter_fields(node):
 
 def astParse():
     if sys.version_info.major != 3:
-        sys.stderr.write(json.dumps({'errType': 'PY_VER'}) + '\n')
+        sys.stderr.write(
+            json.dumps({"errType": "PY_VER"}) + "\n"
+        )
         sys.exit(1)
     inputfile = sys.argv[1]
-    f = open(inputfile, 'r')
+    f = open(inputfile, "r")
     src = f.read()
     try:
         parsed = ast.parse(src)
         ast_json = ast2json(parsed)
         global ast_count
-        sys.stdout.write('{{ "ast_count": {}, "ast_json": {} }}\n'.format(ast_count, ast_json))
-    except:
-        sys.stderr.write(json.dumps({'errType': 'AST_PARSE'}) + '\n')
+        sys.stdout.write(
+            '{{ "ast_count": {}, "ast_json": {} }}\n'.format(
+                ast_count, ast_json
+            )
+        )
+    except Exception as e:
+        print(e)
+        sys.stderr.write(
+            json.dumps({"errType": "AST_PARSE"}) + "\n"
+        )
         sys.exit(1)
     sys.exit(0)
 
